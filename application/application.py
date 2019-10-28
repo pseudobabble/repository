8f4b2b17a597b4de8859e40707a1de1532f81445
pseudobabble <harryjohnson500@gmail.com>
2019-10-28 09:38:05 +0000

    demo changes

--- application/application.py
+++ application/application.py
@@ -11,9 +11,17 @@ user = User(name='Pseudo', fullname='Pseudo Nym', nickname='Nym')
 
 user_repository = UserRepository()
 user_repository.save(user)
-
 user_again = user_repository.get_by_nickname('Nym')
+
+different_user = User(name='An', fullname='An Other User', nickname='Number 2')
+user_repository.save(different_user)
+different_user_again = user_repository.get_by_nickname('Number 2')
+
 
 print(user)
 print(user_again)
+print(user.greet(), ', ', different_user.greet())
 
+print(different_user)
+print(different_user_again)
+print(user_again.greet(), ', ', different_user_again.greet())
